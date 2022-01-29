76a20a0fbafd7e50fd89d24d82bf495ccf83f420
mackmc22 <mackmc22@gmail.com>
2022-01-29 12:59:30 -0500

    added/reorganized code to deal in views. getting error in server

--- blackjack/api/views.py
+++ blackjack/api/views.py
@@ -1,5 +1,6 @@
 import pdb
 import json
+import random
 
 from django.http import HttpResponse, JsonResponse
 
@@ -7,20 +8,16 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, RetrieveAPIView, get_object_or_404
 
-class Game:
-    hand = []
 
-    def deal(self):
-        deck_cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9,
-                      10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K', 'A', 'A', 'A', 'A'
-                      ]
+class Game:
+    deck_cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9,
+                  10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K', 'A', 'A', 'A', 'A'
+                  ]
 
     def __init__(self):
         random.shuffle(self.deck_cards)
 
-    def deal(self):
-        return self.deck_cards.pop()
-
-@api_view(["GET"])
-def deal(request):
-    return JsonResponse(data={'card': game.deal()}, status=status.HTTP_200_OK)
+    @api_view(["GET"])
+    def deal(self, request):
+        dealt_card = self.deck_cards.pop()
+        return JsonResponse(data={'card': dealt_card}, status=status.HTTP_200_OK)
--- blackjack/blackjack/urls.py
+++ blackjack/blackjack/urls.py
@@ -22,6 +22,6 @@ from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("api/game", views.deal, ),
+    path("api/game", views.Game.deal(), ),
 
 ]
