380a0000b44ac7295b0c636b2d6283a845a03877
mackmc22 <mackmc22@gmail.com>
2022-02-02 18:16:08 -0500

    added methods to endpoint, added model info to endpoint, added card variable

--- blackjack/api/models.py
+++ blackjack/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class GameState(models.Model):
-    username = models.TextField()
-    deck = models.TextField()
+    username = models.TextField(null=True)
+    deck = models.TextField(null=True)
     player_hand = models.TextField()
     dealer_hand = models.TextField(null=True)
